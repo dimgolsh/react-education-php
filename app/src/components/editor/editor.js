92c279b7207b7087aae0cb4d7e3f38d0fd68a268
dimon <dimagol64@gmail.com>
2020-04-19 00:21:25 +0300

    add editable not work in opera

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,11 +1,11 @@
 import React from "react";
 import axios from "axios";
-import '../../helpers/iframeLoader.js';
+import "../../helpers/iframeLoader.js";
 
 export default class Editor extends React.Component {
   constructor() {
     super();
-    this.currentPage = 'index.html';
+    this.currentPage = "index.html";
 
     this.state = {
       pageList: [],
@@ -16,22 +16,41 @@ export default class Editor extends React.Component {
   }
 
   componentDidMount() {
-   this.init(this.currentPage);
-
+    this.init(this.currentPage);
   }
 
-  init(page){
-      this.iframe = document.querySelector('iframe');
-      this.open(page);
-      this.loadPageList();
-
+  init(page) {
+    this.iframe = document.querySelector("iframe");
+    this.open(page);
+    this.loadPageList();
   }
-  open(page){
-      this.currentPage = `../${page}`;
-      this.iframe.load(this.currentPage,()=>{
-          console.log(this.currentPage)
-      });
+  open(page) {
+    this.currentPage = `../${page}`;
+    this.iframe.load(this.currentPage, () => {
+      console.log('sss',this.currentPage);
+      const body = this.iframe.contentDocument.body;
+      console.log(body);
+      let textNodes = [];
 
+      function recursy(element) {
+        element.childNodes.forEach(node => {
+          if (node.nodeName === "#text" && node.nodeValue.replace(/\s+/g, '').length > 0) {
+            textNodes.push(node);
+            console.log(node);
+          } else {
+            recursy(node);
+          }
+        });
+      }
+
+      recursy(body);
+      textNodes.forEach(node => {
+          const wrapper = this.iframe.contentDocument.createElement('text-editor');
+          node.parentNode.replaceChild(wrapper, node);
+          wrapper.appendChild(node);
+          wrapper.contentEditable = 'true';
+      })
+    });
   }
 
   loadPageList() {
@@ -46,9 +65,11 @@ export default class Editor extends React.Component {
 
   deletePage(page) {
     axios
-    .post('./api/deletePage.php',{"name":page})
-    .then(this.loadPageList())
-    .catch(()=>{console.log('eroror')})
+      .post("./api/deletePage.php", { name: page })
+      .then(this.loadPageList())
+      .catch(() => {
+        console.log("eroror");
+      });
   }
   render() {
     const { pageList } = this.state;
@@ -56,21 +77,23 @@ export default class Editor extends React.Component {
       return (
         <h1 key={i}>
           {page}
-          <a href="#" onClick={() => this.deletePage(page)}>*</a>
+          <a href="#" onClick={() => this.deletePage(page)}>
+            *
+          </a>
         </h1>
       );
     });
     return (
       <>
-      <iframe src={this.currentPage} frameBorder="0"></iframe>
-        <input
+        <iframe src={this.currentPage} frameBorder="0"></iframe>
+       {/*  <input
           onChange={(e) => {
             this.setState({ newPageName: e.target.value });
           }}
           type="text"
         ></input>
         <button onClick={this.createNewPage}>Add page</button>
-        {pages}
+        {pages} */}
       </>
     );
   }
