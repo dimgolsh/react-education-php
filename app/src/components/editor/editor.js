f98a3800678b1aa7b343b96298ee295a9d69ca42
dimon <dimagol64@gmail.com>
2020-04-18 23:18:35 +0300

    add page react

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -1,24 +1,47 @@
-import React from 'react'
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
+export default class Editor extends React.Component {
+  constructor() {
+    super();
 
-export default class Editor extends React.Component{
-    constructor(){
-        super();
+    this.state = {
+      pageList: [],
+      newPageName: "",
+    };
 
-        this.state = {
-            pageList: [],
-            newPageName: ""
-        }
-    }
+    this.createNewPage = this.createNewPage.bind(this);
+  }
 
-    render(){
+  componentDidMount() {
+    this.loadPageList();
+  }
 
-        return (
-            <>
-                <input type="text"></input>
-                <button>Add page</button>
-            </>
-        )
-    }
+  loadPageList() {
+    axios.get("./api").then((res) => this.setState({ pageList: res.data }));
+  }
+
+  createNewPage() {
+    axios
+      .post("./api/createNewPage.php", { name: this.state.newPageName })
+      .then(this.loadPageList());
+  }
+  render() {
+    const { pageList } = this.state;
+    const pages = pageList.map((page, i) => {
+      return <h1 key={i}>{page}</h1>;
+    });
+    return (
+      <>
+        <input
+          onChange={(e) => {
+            this.setState({ newPageName: e.target.value });
+          }}
+          type="text"
+        ></input>
+        <button onClick={this.createNewPage}>Add page</button>
+        {pages}
+      </>
+    );
+  }
 }
