cd08929a9d9b619eeeadb57290ffa69440ee90a3
dimon <dimagol64@gmail.com>
2020-04-19 17:18:18 +0300

    Save panel uikit

--- app/src/components/editor/editor.js
+++ app/src/components/editor/editor.js
@@ -2,7 +2,9 @@ import React from "react";
 import axios from "axios";
 import "../../helpers/iframeLoader.js";
 import DOMHelper from "../../helpers/dom-helpers.js";
-import EditorText from '../editor-text/editor-text';
+import EditorText from "../editor-text/editor-text";
+import UIkit from "uikit";
+import Spinner from '../spinner/spinner.js';
 
 export default class Editor extends React.Component {
   constructor() {
@@ -12,9 +14,12 @@ export default class Editor extends React.Component {
     this.state = {
       pageList: [],
       newPageName: "",
+      loading: true
     };
 
     this.createNewPage = this.createNewPage.bind(this);
+    this.isLoaded = this.isLoaded.bind(this);
+    this.isLoading = this.isLoading.bind(this);
   }
 
   componentDidMount() {
@@ -23,10 +28,10 @@ export default class Editor extends React.Component {
 
   init(page) {
     this.iframe = document.querySelector("iframe");
-    this.open(page);
+    this.open(page, this.isLoaded);
     this.loadPageList();
   }
-  open(page) {
+  open(page, cb) {
     this.currentPage = page;
     axios
       .get(`../${page}?rnd=${Math.random()}`)
@@ -40,18 +45,24 @@ export default class Editor extends React.Component {
       .then((html) => axios.post("./api/saveTempPage.php", { html }))
       .then(() => this.iframe.load("../temp.html"))
       .then(() => this.enableEditing())
-      .then(()=> this.injectStyles());
+      .then(() => this.injectStyles())
+      .then(cb);
 
     // this.iframe.load(this.currentPage, () => {
 
     // });
   }
 
-  save() {
+  save(onSucceess, onErrorr) {
+      this.isLoading();
     const newDom = this.virtualDom.cloneNode(this.virtualDom);
     DOMHelper.unwrapTextNodes(newDom);
     const html = DOMHelper.serializeDOMToString(newDom);
-    axios.post("./api/savePage.php", { pageName: this.currentPage, html });
+    axios.post("./api/savePage.php", { pageName: this.currentPage, html })
+    .then(onSucceess)
+    .catch(onErrorr)
+    .finally(this.isLoaded)
+    ;
   }
 
   enableEditing() {
@@ -59,25 +70,25 @@ export default class Editor extends React.Component {
       .querySelectorAll("text-editor")
       .forEach((element) => {
         const id = element.getAttribute("nodeid");
-        const virtualElement = this.virtualDom.body.querySelector(`[nodeid="${id}"]`);
+        const virtualElement = this.virtualDom.body.querySelector(
+          `[nodeid="${id}"]`
+        );
 
-        new EditorText(element, virtualElement)
+        new EditorText(element, virtualElement);
       });
 
-  //  console.log(this.virtualDom);
+    //  console.log(this.virtualDom);
   }
 
-  injectStyles(){
-      const style = this.iframe.contentDocument.createElement("style");
-      style.innerHTML = `
+  injectStyles() {
+    const style = this.iframe.contentDocument.createElement("style");
+    style.innerHTML = `
       text-editor:hover {
           outline: 3px solid orange;
           outline-offset: 8px;
       }`;
-      this.iframe.contentDocument.head.appendChild(style);
+    this.iframe.contentDocument.head.appendChild(style);
   }
-
- 
 
   loadPageList() {
     axios.get("./api").then((res) => this.setState({ pageList: res.data }));
@@ -97,36 +108,61 @@ export default class Editor extends React.Component {
         console.log("eroror");
       });
   }
+  isLoaded(){
+    this.setState({
+        loading:false
+    })
+}
+
+  isLoading(){
+      this.setState({
+          loading:true
+      })
+  }
   render() {
-    const { pageList } = this.state;
-    const pages = pageList.map((page, i) => {
-      return (
-        <h1 key={i}>
-          {page}
-          <a href="#" onClick={() => this.deletePage(page)}>
-            *
-          </a>
-        </h1>
-      );
-    });
+
+    const {loading} = this.state;
+    const modal = true;
+
+    let spinner;
+    loading ? spinner = <Spinner active/> : spinner = <Spinner/>
+
     return (
+     
       <>
-        <button
-          onClick={() => {
-            this.save();
-          }}
-        >
-          Click
-        </button>
-        <iframe src={this.currentPage} frameBorder="0"></iframe>
-        {/*  <input
-          onChange={(e) => {
-            this.setState({ newPageName: e.target.value });
-          }}
-          type="text"
-        ></input>
-        <button onClick={this.createNewPage}>Add page</button>
-        {pages} */}
+      {spinner}
+      <iframe src={this.currentPage} frameBorder="0"></iframe>
+      
+        <div className="panel">
+          <button
+            className="uk-button uk-button-primary"
+            uk-toggle="target: #my-id" 
+          >
+            Click
+          </button>
+        </div>
+        <div id="my-id" uk-modal={modal.toString()}>
+          <div className="uk-modal-dialog uk-modal-body">
+            <h2 className="uk-modal-title">Save</h2>
+            <p>Вы уверены?</p>
+            <button className="uk-button uk-button-primary  uk-modal-close" type="button"
+              onClick={() => {
+                this.save(()=>{
+                    UIkit.notification({message: 'Notification message',status: 'success'})
+                });
+              },
+              () => {
+                this.save(()=>{
+                    UIkit.notification({message: 'Error',status: 'danger'})
+                });
+              }
+            }
+            >Да</button>
+            <button className="uk-button uk-button-secondary uk-modal-close" type="button">Нет</button>
+          </div>
+        </div>
+
+      
       </>
     );
   }
